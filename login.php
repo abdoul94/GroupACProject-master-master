8cb88fcab04aa8a0b0b9bb9ee9d96e879ae9f407
aonaas <32803624+aonaas@users.noreply.github.com>
2019-02-15 20:22:47 +0000

    Initial commit

---
+++ login.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Multilevel Login Form </tilte>
+</head>
+<body>
+<form method="POST">
+    <table>
+<tr>
+<td>User Type</td>
+<td><select name="type">
+<option value="1"select user type</option>
+<option value="Admin">Admin </option>
+<option value="">User</option>
+</select></td>
+</tr>
+<tr> Username</tr>
+<td><input type="text" name="Username" placeholder="Username"></td>
+        </tr>
+
+        <tr>
+
+            <td>Password</td>
+        <td>input type="password" name="pwd" placeholder="password></td>
+        </tr>
+        <tr>
+
+
+        <td><input type="password" name="pwd" placeholder="password"></td>
+            </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input> type="submit" name="submit" value ="Login"></td>
+        </tr>
+
+</table>
+</form>
+
+</body>
+</html>
